9f47f7b582082ddf1bd62118650d85160c2836c0
Hùng <v.hungvq10@vinid.net>
2020-04-20 09:37:08 +0700

    Add docker

---
+++ docker/go/src/go.mod
@@ -0,0 +1,8 @@
+module go.sqli
+
+go 1.13
+
+require (
+	github.com/gin-gonic/gin v1.6.2
+	github.com/jinzhu/gorm v1.9.12
+)
---
+++ docker/mysql/test.sql
@@ -0,0 +1,5 @@
+Use product;
+
+Create table `product` (price int, code varchar(20));
+
+Insert into product (price, code) values (1331, 'LEET');
